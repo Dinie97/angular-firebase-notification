4ab596f154dd54380f8424d29f82c89a8dd27515
Dinie <diniesyahirah9@gmail.com>
2023-07-21 14:52:45 +0800

    add-firebase-js

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+importScripts("https://www.gstatic.com/firebasejs/12.4.5/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.4.5/firebase-messaging.js"
+);
+
+firebase.initializeApp({});
